7228fae28d87e91b8255c4ad687d5fe48581187f
GIOVANNI PERAZZOLI SILVA PIROLLA <giovanni.pirolla@etec.sp.gov.br>
2026-05-21 11:05:33 -0300

    Definindo as imagens, text input e botões

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Image, Pressable, TextInput } from 'react-native';
 
 export default function App() {
+  const [gas, setGas] = useState('');
+  const [etanol, setEtanol] = useState('');
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text
+      style = {styles.titulo}
+      >Gasolina ou Álcool?</Text>
+      <Image 
+      style = {styles.imagem_posto}
+      source={require('./assets/posto-gasolina.jpg')}/>
+      <TextInput
+      style = {styles.precos}
+      placeholder='Digite o preço da Gasolina: '
+      />
+      <TextInput
+      style = {styles.precos}
+      placeholder='Digite o preço do Etanol: '
+      />
+      <Pressable
+      style = {}
+      >
+        <Text>Verificar Vantagem</Text>
+      </Pressable>
     </View>
   );
 }
@@ -13,8 +34,30 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f7f0a6',
     alignItems: 'center',
-    justifyContent: 'center',
   },
+
+  titulo: {
+    fontSize: 26,
+    padding: 42,
+    fontFamily: 'fantasy',
+    backgroundColor: '#f8cb00',
+  },
+
+  precos: {
+    fontFamily: 'fantasy',
+    fontSize: 15,
+    backgroundColor: '#f8cb00',
+    borderColor: '#ff0000',
+    borderRadius: 15,
+    padding: 10,
+    margin: 10,
+    textAlign: 'left',
+  },
+
+  imagem_posto: {
+    height: 200,
+    width: 300,
+  }
 });

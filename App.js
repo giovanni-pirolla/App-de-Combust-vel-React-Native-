219cf753aba0208b9d47eed17811395ac15d62fc
GIOVANNI PERAZZOLI SILVA PIROLLA <giovanni.pirolla@etec.sp.gov.br>
2026-05-27 11:19:19 -0300

    Adicionando ResultCard, função para verificar vantagem e configurando adequadamente os botões e text inputs, juntamente da estilização

--- App.js
+++ App.js
@@ -1,32 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
 import { StyleSheet, Text, View, Image, Pressable, TextInput } from 'react-native';
+import ResultCard from './components/ResultCard';
 
 export default function App() {
   const [gas, setGas] = useState('');
   const [etanol, setEtanol] = useState('');
+  const [resultado, setResultado] = useState('');
+  const [porcentagem, setPorcentagem] = useState('');
+
+  function verificarVantagem() {
+    setPorcentagem(((etanol / gas) * 100).toFixed(2));
+
+    if (porcentagem <= 70) {
+      setResultado('Etanol');
+    } else {
+      setResultado('Gasolina');
+    }
+  }
 
   return (
     <View style={styles.container}>
-      <Text
-      style = {styles.titulo}
-      >Gasolina ou Álcool?</Text>
+      <Text style={styles.titulo}>Gasolina ou Álcool?</Text>
       <Image 
-      style = {styles.imagem_posto}
-      source={require('./assets/posto-gasolina.jpg')}/>
-      <TextInput
-      style = {styles.precos}
-      placeholder='Digite o preço da Gasolina: '
+        style={styles.imagem_posto}
+        source={require('./assets/posto-gasolina.jpg')}
       />
       <TextInput
-      style = {styles.precos}
-      placeholder='Digite o preço do Etanol: '
+        style={styles.precos}
+        placeholder='Digite o preço da Gasolina: '
+        keyboardType='numeric'
+        value={gas}
+        onChangeText={(texto) => setGas(texto)}
       />
-      <Pressable
-      style = {}
-      >
-        <Text>Verificar Vantagem</Text>
+      <TextInput
+        style={styles.precos}
+        placeholder='Digite o preço do Etanol: '
+        keyboardType='numeric'
+        value={etanol}
+        onChangeText={(texto) => setEtanol(texto)}
+      />
+      <Pressable onPress={verificarVantagem}>
+        <Text style={styles.botao}>Verificar Vantagem</Text>
       </Pressable>
+      <ResultCard gas={gas} etanol={etanol} resultado={resultado} porcentagem={porcentagem}/>
     </View>
   );
 }
@@ -37,26 +54,36 @@ const styles = StyleSheet.create({
     backgroundColor: '#f7f0a6',
     alignItems: 'center',
   },
-
   titulo: {
     fontSize: 26,
     padding: 42,
     fontFamily: 'fantasy',
     backgroundColor: '#f8cb00',
   },
-
   precos: {
     fontFamily: 'fantasy',
     fontSize: 15,
     backgroundColor: '#f8cb00',
     borderColor: '#ff0000',
+    borderWidth: 1,
     borderRadius: 15,
     padding: 10,
     margin: 10,
+    width: 280,
     textAlign: 'left',
   },
-
+  botao: {
+    fontFamily: 'fantasy',
+    fontSize: 15,
+    backgroundColor: '#ff0000',
+    borderRadius: 15,
+    padding: 10,
+    margin: 10,
+    width: 280,
+    textAlign: 'center',
+  },
   imagem_posto: {
+    top: -10,
     height: 200,
     width: 300,
   }
---
+++ components/ResultCard.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function ResultCard({ gas, etanol, resultado, porcentagem }) {
+  return (
+    <View style={styles.card}>
+      <Text style={styles.destaque}>
+        Abasteça com {resultado}.
+      </Text>
+      <Text style={styles.texto}>
+        O Etanol está custando {porcentagem}% do preço da Gasolina.
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: '#f8cb00',
+    padding: 16,
+    borderRadius: 10,
+    marginTop: 20,
+    width: 280,
+  },
+  texto: {
+    fontSize: 14,
+    fontFamily: 'fantasy',
+  },
+  destaque: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#ff0000',
+    marginTop: 5,
+    marginBottom: 15,
+  }
+});
